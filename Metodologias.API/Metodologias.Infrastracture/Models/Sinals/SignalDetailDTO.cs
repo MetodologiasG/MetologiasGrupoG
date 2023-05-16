137d6949f68e401c32983d9750cbdd80e79a216b
HenriqueOliveira29 <henrique.olive29@gmail.com>
2023-05-16 17:52:10 +0100

    Obter Sinal

--- Metodologias.API/Metodologias.Infrastracture/Models/Sinals/SignalDetailDTO.cs
+++ Metodologias.API/Metodologias.Infrastracture/Models/Sinals/SignalDetailDTO.cs
@@ -1,4 +1,5 @@
-﻿using Metodologias.Infrastracture.Models.TemporalInformation;
+﻿using Metodologias.Infrastracture.Entities;
+using Metodologias.Infrastracture.Models.TemporalInformation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,26 @@ namespace Metodologias.Infrastracture.Models.Sinals
 {
     public class SignalDetailDTO
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Ref { get; set; }
-        public string Value { get; set; }
+        public int Value { get; set; }
         public bool SetSignal { get; set; }
         public List<TemporalInformationListDTO> TemporalInformationLists { get; set; }
+
+        public SignalDetailDTO(Signal signal)
+        {
+            this.Id = signal.Id;
+            this.Ref = signal.Ref;
+            this.Value = signal.Value;
+            if (signal.TemporalInformation.Where(t => t.RemoveDate == null).Any() == true)
+            {
+                this.SetSignal = false;
+            }
+            else
+            {
+                this.SetSignal = true;
+            }
+            this.TemporalInformationLists = signal.TemporalInformation.Select(t => new TemporalInformationListDTO(t)).ToList();
+        }
     }
 }
